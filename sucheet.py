a472040b2d1566a36718b7e579a9a1079cdb810a
manojmirajakar9-lgtm <manojmirajakar9@gmail.com>
2026-03-13 16:13:43 +0530

    Add files via upload

---
+++ sucheet.py
@@ -0,0 +1,14 @@
+# creating two sets
+set1=set()
+set2=set()
+
+# adding elements to set1
+  for i in range (1,6):
+	set1.add (i)
+
+# adding elements to set2
+  for i in range (3,9):
+	set2.add (i)
+print("set1= ",set)
+print("set2= ",set)
+print("\n")
