1120a29b8ce8ef9956208e6f6283ceba7b23cd3b
manojmirajakar9-lgtm <manojmirajakar9@gmail.com>
2026-03-13 16:14:31 +0530

    Add files via upload

---
+++ akash.py
@@ -0,0 +1,14 @@
+class node:
+      def__int__(self.data):
+            self.data=data
+            self.next=none
+            
+class singlylinkedlist:
+	def__init__(self):
+	 self.head=none
+	
+	def prepend(self,data)
+	 new_node=note(data)
+	 new_node next=self.head
+	 self.head=new-node
+    
